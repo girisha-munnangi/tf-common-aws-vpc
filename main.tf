bbcce497ba5f9b1febb3cf5413558599d4e83e0d
Girisha <girishamunnangi@gmail.com>
2026-03-08 01:13:42 +0530

    terraform

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
     count = length(var.private_subnet_cidrs)
     vpc_id = aws_vpc.this.id
-    cidr_block = var.public_subnet_cidrs[count.index]
+    cidr_block = var.private_subnet_cidrs[count.index]
     availability_zone = local.az_names[count.index]
     tags = merge(
         local.common_tags,
